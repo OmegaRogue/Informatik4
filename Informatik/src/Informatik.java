e5140c6649591098d857e828aba7d3d3d2da2c60
OmegaRogue <OmegaRogue@users.noreply.github.com>
2018-09-28 15:16:31 +0200

    Hopefully fixed issues

--- Informatik/src/Informatik.java
+++ Informatik/src/Informatik.java
@@ -44,31 +44,4 @@ public class Informatik {
 
 }
 
-public class informatik {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a = {32,17,24,7,8,12,4};
-		System.out.print(a[0]);
-		for (int i = 1; i < a.length; i++) {
-			System.out.print("; "+a[i]);
-		}
-		
-	}
-	
-
-}
-public class informatik {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int[] a = {32,17,24,7,8,12,4};
-		System.out.print(a[0]);
-		for (int i = 1; i < a.length; i++) {
-			System.out.print("; "+a[i]);
-		}
-		
-	}
-	
-
-}
