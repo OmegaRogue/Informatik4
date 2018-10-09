e403011f726635ad754e1edb40af15ba4b1dba4e
OmegaRogue <OmegaRogue@users.noreply.github.com>
2018-10-09 08:31:27 +0200

    changes

---
+++ Informatik/src/omegaRogue/Baum.java
@@ -0,0 +1,71 @@
+package omegaRogue;
+
+
+/**
+ * Eigene Baumimplementation zu Übungszwecken im Informatikuntericht 4std G11.
+ * 
+ * 
+ * @since 05.10.2018
+ *
+ */
+public class Baum 
+{
+	public CharImg output = new CharImg();
+	BaumElement root;
+	
+	/**
+	 * Die ist der Konstruktor der Liste.
+	 * @param startWert Dies ist das Startelement der Liste. 
+	 */
+	public Baum(String startWert)
+	{
+		root = new BaumElement(startWert);
+	}
+	public String toString() {
+		String ausgabe = "";
+		ausgabe += root.inhalt;
+		ausgabe += CharImg.generate("000301000602");
+		return ausgabe;
+	}
+}	
+
+
+
+
+
+
+	/*
+	/**
+	 * Diese Funktion fügt ein neues Baumelement hinzu. 
+	 * @param neuerWert Der Inhalt für das hinzuzufügende Baumelement.
+	 *
+	public void add(String neuerWert)
+	{
+		// Zeilenkommentar
+		BaumElement neuesElement = new ListenElement(neuerWert);
+		ListenElement e = startElement;
+		while (e.nachfolger!=null)
+		{
+			e=e.nachfolger;
+		}
+		e.setNachfolger(neuesElement);
+	}
+	
+	/**
+	 * Diese Funktion gibt den Inhalt der Liste auf der Konsole aus.
+	 *
+	public void listeAusgeben()
+	{
+	/*
+	 *  Blockkommentar
+	 * 
+	    ListenElement e = startElement;
+		while (e.nachfolger!=null)
+		{
+			System.out.println(e.inhalt);
+			e=e.nachfolger;
+		}
+		System.out.println(e.inhalt); 
+	}
+	*/
+	
---
+++ Informatik/src/omegaRogue/BaumElement.java
@@ -0,0 +1,21 @@
+package omegaRogue;
+
+import java.util.ArrayList;
+
+public class BaumElement 
+{
+	public String inhalt;
+	public ArrayList <BaumElement> nachfolger;
+	
+	public BaumElement(String wert)
+	{
+		inhalt = wert;
+	}
+	
+	public void addNachfolger(String wert)
+	{
+		BaumElement neuerNachfolger = new BaumElement(wert);
+		nachfolger.add(neuerNachfolger);
+	}
+	
+}
